abdabb21cc4bca5ab03f12afb8551f9df0fbfb18
fong22e <fong22e@mtholyoke.edu>
2019-04-26 17:37:21 -0400

    added script for building CPL

---
+++ ef-tests/buildcpl.sh
@@ -0,0 +1,18 @@
+pushd /home/cpl/prov-cpl
+
+# uninstall CPL
+sudo make uninstall
+
+# clean CPL
+sudo make clean
+sudo make distclean
+sudo make messclean
+
+# install CPL
+sudo make release
+sudo make install
+
+# install r bindings
+sh bindings/r/install-bindings-r.sh
+
+popd
